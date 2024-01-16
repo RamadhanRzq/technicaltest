42cdb925db6d9fc69a252df1625767a552f0edae
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2024-01-16 12:14:20 +0700

    init project frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <header className="App-header">Learn React</header>
+    </div>
+  );
+}
